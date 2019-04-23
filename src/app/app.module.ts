6ca72b44582e4969f1cdb021c9e806e6e4e15085
Hernane <linehcm@hotmail.com>
2019-04-23 17:55:46 -0400

    Correções em pacotes
    Corrigido alguns pacotes do PrimeNG.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutes } from './app.router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MenuModule } from 'primeng/menu';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(AppRoutes,)
+    MenuModule,
+    PanelMenuModule,
+    ContextMenuModule,
+    MenubarModule,
+    ButtonModule,
+    InputTextModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
